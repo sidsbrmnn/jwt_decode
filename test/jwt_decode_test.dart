46ee5be50caad966962a5375b1ef1bf8dfadc797
Varun S Athreya <varun.sathreya@gmail.com>
2020-12-22 02:32:44 +0530

    Added types in test var

--- test/jwt_decode_test.dart
+++ test/jwt_decode_test.dart
@@ -2,11 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:jwt_decode/jwt_decode.dart';
 
 void main() {
-  final token =
+  final String token =
       'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJKV1QgRGVjb2RlIiwiaWF0IjoxNjA4NTgxNzczLCJleHAiOjE2NDAxMTc3NzMsImF1ZCI6Ind3dy5qd3RkZWNvZGUuY29tIiwic3ViIjoiQSBzYW1wbGUgSldUIiwibmFtZSI6IlZhcnVuIFMgQXRocmV5YSIsImVtYWlsIjoidmFydW4uc2F0aHJleWFAZ21haWwuY29tIiwicm9sZSI6IkRldmVsb3BlciJ9.vXE9ogUeMMsOTz2XQYHxE2hihVKyyxrhi_qfhJXamPQ';
-  final invalidToken =
+  final String invalidToken =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ';
-  final payload = {
+  Map<String, dynamic> payload = {
     'iss': 'JWT Decode',
     'iat': 1608581773,
     'exp': 1640117773,
